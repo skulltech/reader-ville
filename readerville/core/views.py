799c6a3abe89120b5061117f6de3171439325371
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-03-04 03:12:12 +0530

    Seperated the views. Updated requirements.txt.

--- readerville/core/views.py
+++ readerville/core/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
-from .forms import ProfileForm, LoginForm, BookForm
-from .models import UserProfile, Library
+from accounts.forms import LoginForm
 
-# Create your views here.
 
 def home(request):
     login_form = LoginForm(request.POST or None)
@@ -13,23 +11,3 @@ def home(request):
         if user is not None:
             login(request, user)
     return render(request, 'home.html', context={"login_form": login_form})
-
-
-
-@login_required(login_url='/accounts/login')
-def view_library(request):
-    try:
-        library = request.user.library
-    except Library.DoesNotExist:
-        library = Library(user=request.user)
-        library.save()
-
-    books = library.book_set.all()
-
-    book_form = BookForm(request.POST or None)
-    if book_form.is_valid():
-        book = book_form.save(commit=False)
-        book.library = library
-        book.save()
-
-    return render(request, 'profile/view-library.html', context={"book_form": book_form, "books": books})
--- readerville/libraries/views.py
+++ readerville/libraries/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import BookForm
+from .models import Library
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login')
+def view_library(request):
+    try:
+        library = request.user.library
+    except Library.DoesNotExist:
+        library = Library(user=request.user)
+        library.save()
+
+    books = library.book_set.all()
+
+    book_form = BookForm(request.POST or None)
+    if book_form.is_valid():
+        book = book_form.save(commit=False)
+        book.library = library
+        book.save()
+
+    return render(request, 'profile/view-library.html', context={"book_form": book_form, "books": books})
